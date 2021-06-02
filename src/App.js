5ec8cbf696cbccdb5f85fc87d5d8f67b5e5bb8e3
ashulmes <ashulmes@gmail.com>
2021-06-02 22:31:01 +0100

    Built initial search engine functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World!</header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center">Define It</h1>
+          <h2 className="text-center">What would you like to search for?</h2>
+        </header>
+        <main>
+          <SearchEngine />
+        </main>
+      </div>
     </div>
   );
 }
