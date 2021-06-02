d5b3a61c43240b9763209d9217388c6db3ed922b
ashulmes <ashulmes@gmail.com>
2021-06-02 22:57:40 +0100

    Installed Axios and built dictionary API integration.

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}...`);
+
+    // API documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
