985117a0f16ae56a3840efa5511b648e34a08acc
ashulmes <ashulmes@gmail.com>
2021-06-12 14:49:04 +0100

    Rendered phonetics in new component.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function SearchResults(props) {
   if (props.searchResults) {
     return (
       <div className="SearchResults">
         <h2>{props.searchResults.word}</h2>
+        {props.searchResults.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
+        <br />
         {props.searchResults.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
